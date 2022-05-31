4bb6faa298a9fd547e026dd18fbf573f070273d3
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2022-05-31 18:57:32 -0300

    Implement HTML templating, create page Home

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-  return '<h1>Hello there</h1>'
+  return render_template('home.html')
 
 if __name__ == "__main__":
   app.run(debug=True)
