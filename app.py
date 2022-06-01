6704d0e31865931dadd2d5bc92e396981688f015
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2022-05-31 21:43:43 -0300

    Create test database usage

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
 from flask import Flask, render_template
+import psycopg2
+
 app = Flask(__name__)
+
+@app.route('/init')
+def init():
+  conn = psycopg2.connect(dbname="links-app_db", user="postgres", password="postgres", host="postgres")
+
+  cur = conn.cursor()
+
+  cur.execute("CREATE TABLE something (name TEXT)")
+
+  conn.commit()
+  
+  return 'OK', 200
 
 @app.route('/')
 def home():
