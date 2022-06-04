f1270efe948b1b92db43373b229e1668a6d19737
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2022-06-03 21:53:14 -0300

    Update project to module approach

---
+++ app/main.py
@@ -0,0 +1,40 @@
+import psycopg2
+import os
+import sys
+
+from dotenv import load_dotenv
+from flask import Flask, render_template
+
+def create_app():
+  app = Flask(__name__) 
+
+  if not os.path.isfile(".env"):
+    print("\nNo '.env' file found! Exiting...\n")
+    sys.exit(4)
+
+  load_dotenv()
+
+  @app.route('/something')
+  def something():
+    conn = psycopg2.connect(dbname=os.environ['DB_DATABASE'], user=os.environ['DB_USER'], password=os.environ['DB_PASSWORD'], host=os.environ['DB_HOST'])
+
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM something")
+
+    print(cur.fetchall())
+
+    cur.close()
+
+    conn.close()
+    
+    return 'OK', 200
+
+  @app.route('/')
+  def home():
+    return render_template('home.html')
+
+  if __name__ == "__main__":
+    app.run(debug=True)
+
+  return app
