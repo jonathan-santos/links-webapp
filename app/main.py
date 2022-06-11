1dc0cbd9c8342ca91506e2d88313964df7a04baf
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2022-06-11 19:11:58 -0300

    Implement form usage and validation for pages login and register

--- app/main.py
+++ app/main.py
@@ -1,9 +1,21 @@
 import psycopg2
+import re
 import os
 import sys
 
+from flask_login import LoginManager
 from dotenv import load_dotenv
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for
+
+def get_db_connection():
+    conn = psycopg2.connect(
+      dbname=os.environ['DB_DATABASE'],
+      user=os.environ['DB_USER'],
+      password=os.environ['DB_PASSWORD'],
+      host=os.environ['DB_HOST']
+    )
+
+    return conn
 
 def create_app():
   app = Flask(__name__) 
@@ -14,32 +26,81 @@ def create_app():
 
   load_dotenv()
 
-  @app.route('/something')
-  def something():
-    conn = psycopg2.connect(dbname=os.environ['DB_DATABASE'], user=os.environ['DB_USER'], password=os.environ['DB_PASSWORD'], host=os.environ['DB_HOST'])
+  login_manager = LoginManager()
+  login_manager.init_app(app)
 
-    cur = conn.cursor()
-
-    cur.execute("SELECT * FROM links")
-
-    print(cur.fetchall())
-
-    cur.close()
-
-    conn.close()
-    
-    return 'OK', 200
+  @login_manager.user_loader
+  def load_user(user_id):
+    return None #User.get(user_id)
 
   @app.route('/')
   def index():
     return render_template('index.html')
 
-  @app.route('/register')
+  @app.route('/register', methods=['GET', 'POST'])
   def register():
-    return render_template('register.html')
+    if (request.method == 'GET'):
+      return render_template('register.html')
+    
+    username = request.form.get('username', '')
+    email = request.form.get('email', '')
+    password = request.form.get('password', '')
 
-  @app.route('/login')
+    if not username:
+        return ("username cannot be empty", 400)
+
+    if len(username) < 2:
+      return ("username length cannot be lower than 2", 400)
+
+    if len(username) > 50:
+      return ("username length cannot be greater than 50", 400)
+
+    if not email:
+        return ("email cannot be empty", 400)
+
+    if not re.match('^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$', email):
+      return ("email is invalid", 400)
+
+    if len(email) > 50:
+      return ("email length cannot be greater than 255", 400)
+
+    if not password:
+      return ("password cannot be empty", 400)
+
+    if len(password) < 2:
+      return ("password length cannot be lower than 2", 400)
+
+    if len(password) > 50:
+      return ("password length cannot be greater than 50", 400)
+
+    return ('OK', 200)
+
+  @app.route('/login', methods=['GET', 'POST'])
   def login():
-    return render_template('login.html')
+    if (request.method == 'GET'):
+      return render_template('login.html')
+    
+    email = request.form.get('email', '')
+    password = request.form.get('password', '')
+
+    if not email:
+        return ("email cannot be empty", 400)
+
+    if not re.match('^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$', email):
+      return ("email is invalid", 400)
+
+    if len(email) > 50:
+      return ("email length cannot be greater than 255", 400)
+
+    if not password:
+      return ("password cannot be empty", 400)
+
+    if len(password) < 2:
+      return ("password length cannot be lower than 2", 400)
+
+    if len(password) > 50:
+      return ("password length cannot be greater than 50", 400)
+
+    return ('OK', 200)
 
   return app
