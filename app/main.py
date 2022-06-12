5ed7010877269a88ab9ad1c6bed2c3ffd529a14a
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2022-06-12 12:25:54 -0300

    Implement user login and register functionality

--- app/main.py
+++ app/main.py
@@ -3,19 +3,35 @@ import re
 import os
 import sys
 
-from flask_login import LoginManager
+from http import HTTPStatus
+from flask_login import LoginManager, login_required, login_user, logout_user, current_user
 from dotenv import load_dotenv
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, redirect, render_template, request, url_for, abort
+from werkzeug.security import check_password_hash, generate_password_hash
+
+class User ():
+  def __init__(self, id, username, email):
+    self.id = id
+    self.username = username
+    self.email = email
+    self.is_authenticated = True
+    self.is_active = True
+    self.is_anonymous = False
+
+
+  def get_id(self):
+    return str(self.id)
+
 
 def get_db_connection():
-    conn = psycopg2.connect(
+    con = psycopg2.connect(
       dbname=os.environ['DB_DATABASE'],
       user=os.environ['DB_USER'],
       password=os.environ['DB_PASSWORD'],
       host=os.environ['DB_HOST']
     )
 
-    return conn
+    return con
 
 def create_app():
   app = Flask(__name__) 
@@ -26,19 +42,46 @@ def create_app():
 
   load_dotenv()
 
+  app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+
   login_manager = LoginManager()
   login_manager.init_app(app)
 
+  @login_manager.unauthorized_handler
+  def unauthorized():
+    if request.blueprint == 'api':
+        abort(HTTPStatus.UNAUTHORIZED)
+
+    return redirect('/login')
+
   @login_manager.user_loader
   def load_user(user_id):
-    return None #User.get(user_id)
+    con = get_db_connection()
+    cur = con.cursor()
+
+    cur.execute("SELECT * FROM users WHERE id = %s", (user_id,))
+    user = cur.fetchone()
+
+    if not user:
+      return None
+
+    return User(user[0], user[1], user[2])
 
   @app.route('/')
   def index():
     return render_template('index.html')
 
+  @app.route('/logout')
+  @login_required
+  def logout():
+    logout_user()
+    return redirect('/')
+
   @app.route('/register', methods=['GET', 'POST'])
   def register():
+    if current_user.is_authenticated:
+      return redirect('/')
+    
     if (request.method == 'GET'):
       return render_template('register.html')
     
@@ -73,10 +116,48 @@ def create_app():
     if len(password) > 50:
       return ("password length cannot be greater than 50", 400)
 
-    return ('OK', 200)
+    con = get_db_connection()
+    cur = con.cursor()
+
+    cur.execute("SELECT username FROM users")
+    same_usernames = cur.fetchone() or []
+
+    if (not len(same_usernames) == 0):
+      cur.close()
+      con.close()
+      return ("username already used", 400)
+
+    cur.execute("SELECT email FROM users")
+    same_emails = cur.fetchone() or []
+
+    if (not len(same_emails) == 0):
+      cur.close()
+      con.close()
+      return ("email already used", 400)
+
+    password_hash = generate_password_hash(password)
+
+    cur.execute("""
+      INSERT INTO users (username, email, password, created_at)
+      VALUES (%s, %s, %s, NOW())
+      RETURNING id
+    """, (username, email, password_hash))
+
+    user_id = cur.fetchone()[0]
+
+    login_user(User(user_id, username, email))
+
+    con.commit()
+    cur.close()
+    con.close()
+
+    return redirect('/')
 
   @app.route('/login', methods=['GET', 'POST'])
   def login():
+    if current_user.is_authenticated:
+      return redirect('/')
+    
     if (request.method == 'GET'):
       return render_template('login.html')
     
@@ -101,6 +182,22 @@ def create_app():
     if len(password) > 50:
       return ("password length cannot be greater than 50", 400)
 
-    return ('OK', 200)
+    con = get_db_connection()
+    cur = con.cursor()
+
+    cur.execute("SELECT * FROM users WHERE email =  %s", (email,))
+    user = cur.fetchone()
+
+    if not user:
+      return ("email not found", 400)
+
+    if not check_password_hash(user[3], password):
+      return ("wrong password", 400)
+
+    login_user(User(user[0], user[1], user[2]))
+
+    cur.close()
+    con.close()
+    return redirect('/')
 
   return app
