b360a6d8b61e88165440d6a91fe0fb32a3c7aba7
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2022-06-23 19:36:26 -0300

    Add tag field in link creation

--- app/routes/links_new/router.py
+++ app/routes/links_new/router.py
@@ -2,7 +2,8 @@ from flask import Blueprint, render_template, request
 from flask_login import login_required, current_user
 from re import match
 
-from ...db import DB
+from app.db import DB
+from app.utils import clean_string
 
 links_new = Blueprint(
   'links_new', __name__,
@@ -16,6 +17,7 @@ def links_new_page():
     return render_template('links_new.html')
   
   link = request.form.get('link', '')
+  tag = request.form.get('tag', '')
 
   if not link:
       return ("link cannot be empty", 400)
@@ -23,11 +25,41 @@ def links_new_page():
   if not match("^(http(s)?:\/\/)+[\w\-\._~:\/?#[\]@!\$&'\(\)\*\+,;=.]+$", link):
     return ("links is invalid", 400)
 
-  db = DB("""
+  db = DB()
+
+  tag_id = 1 # untagged
+
+  if tag:
+    if len(tag) < 2:
+      db.close()
+      return ("tag length cannot be lower than 2", 400)
+
+    if len(tag) > 50:
+      db.close()
+      return ("tag length cannot be greater than 50", 400)
+
+    db.execute("SELECT id FROM tags WHERE title = %s", [tag])
+
+    tag_result = db.getOne()
+
+    if tag_result == None:
+      db.execute("""
+        INSERT INTO tags (title, created_at)
+        VALUES (%s, NOW())
+        RETURNING id
+      """, [clean_string(tag)])
+
+      tag_result = db.getOne()
+
+      db.save()
+
+    tag_id = tag_result[0]
+
+  db.execute("""
     INSERT INTO links (url, tag_id, user_id, created_at, updated_at)
     VALUES (%s, %s, %s, NOW(), NOW())
     RETURNING id
-  """, [link, 1, current_user.id])
+  """, [clean_string(link), tag_id, current_user.id])
 
   db.save()
   db.close()
