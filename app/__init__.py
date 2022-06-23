d9eabd770534d25d82c4131debcf664e576e9a27
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2022-06-23 19:54:04 -0300

    Refactor route blueprint registration in app entrypoint

--- app/__init__.py
+++ app/__init__.py
@@ -4,15 +4,7 @@ from flask_login import LoginManager
 from .config import load_config
 from .auth import config_auth
 
-from .routes.index.router import index
-from .routes.register.router import register
-from .routes.login.router import login
-from .routes.logout.router import logout
-from .routes.links_new.router import links_new
-from .routes.user_links.router import user_links
-from .routes.tag.router import tag
-from .routes.tags.router import tags
-from .routes.tags_new.router import tags_new
+from .routes import routes
 
 login_manager = LoginManager()
 
@@ -23,15 +15,8 @@ def init_app():
   login_manager.init_app(app)
 
   with app.app_context():
-    app.register_blueprint(index)
-    app.register_blueprint(register)
-    app.register_blueprint(login)
-    app.register_blueprint(logout)
-    app.register_blueprint(links_new)
-    app.register_blueprint(user_links)
-    app.register_blueprint(tag)
-    app.register_blueprint(tags)
-    app.register_blueprint(tags_new)
+    for route in routes:
+      app.register_blueprint(route)
 
     config_auth(login_manager)
 
---
+++ app/routes/__init__.py
@@ -0,0 +1,21 @@
+from .index.router import index
+from .register.router import register
+from .login.router import login
+from .logout.router import logout
+from .links_new.router import links_new
+from .user_links.router import user_links
+from .tag.router import tag
+from .tags.router import tags
+from .tags_new.router import tags_new
+
+routes = [
+  index,
+  register,
+  login,
+  logout,
+  links_new,
+  user_links,
+  tag,
+  tags,
+  tags_new
+]
