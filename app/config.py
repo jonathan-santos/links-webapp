2d9743fd50d50b66e2fc18f034a7c6a26e6f4974
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2022-07-04 19:28:22 -0300

    Remove python-dotenv from requirements.txt, add .env file loading to docker and remove .env file loading from config.py

--- app/config.py
+++ app/config.py
@@ -1,12 +1,4 @@
-from os import path, environ
-from sys import exit
-from dotenv import load_dotenv
+from os import environ
 
 def load_config(app):
-  if not path.isfile(".env"):
-      print("\nNo '.env' file found.\n Exiting...\n")
-      exit(4)
-
-  load_dotenv()
-
   app.config['SECRET_KEY'] = environ['SECRET_KEY']
